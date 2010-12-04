7abbf4ebd28fb7753d2d603a749900c62c4d8357
Yihui Xie <xie@yihui.name>
2010-12-04 15:54:18 -0600

    using the Rscript command now; a complete list of my packages

--- github-update.R
+++ github-update.R
@@ -1,16 +1,45 @@
-## Rscript configuration/github-update.R
-pkg = c('animation', 'fun', 'R2SWF', 'formatR', 'iBUGS', 'Rd2roxygen')
+## Usage
+## update: Rscript configuration/github-update.R
+## clone: Rscript configuration/github-update.R clone
+## install: Rscript configuration/github-update.R install
 
-if (FALSE) {
-	for (i in pkg) {
-		system(sprintf('git clone git@github.com:yihui/%s.git', i))
-	}
-}
-if (.Platform$OS.type == 'windows') {
-	Sys.setenv(PATH = paste(readRegistry('Software\\TortoiseGit', 'HCU')$MSysGit, Sys.getenv('PATH'), sep = ';'))
-	system = shell
+pkg = list(yihui = c('Rd2roxygen', 'animation', 'fun', 'R2SWF', 'formatR', 'iBUGS'),
+           ggobi = c('plumbr', 'qtbase', 'qtpaint', 'tourr', 'qtutils', 'mosaiq'))
+
+## clean up
+unlink(sprintf('%s.roxygen', unlist(pkg)), TRUE)
+
+## clone repositories
+if ('clone' %in% commandArgs(TRUE)) {
+    for (i in names(pkg)) {
+        sapply(pkg[[i]], function(x) {
+            timestamp()
+            message('clone: ', x)
+            system(sprintf('git clone git@github.com:%s/%s.git', i, x))
+        }
+               )
+    }
+} else {
+
+    ## under Windows, we might need to set the PATH variable
+    if (.Platform$OS.type == 'windows') {
+        Sys.setenv(PATH = paste(readRegistry('Software\\TortoiseGit', 'HCU')$MSysGit, Sys.getenv('PATH'), sep = ';'))
+        system = shell
+    }
+
+    ## update the packages
+    for (i in unlist(pkg)) {
+        timestamp()
+        message('updating: ', i)
+        system(sprintf('cd %s && git pull', i))
+    }
 }
 
-for (i in pkg) {
-	system(sprintf('cd %s && git pull', i))
+## install R packages
+if ('install' %in% commandArgs(TRUE)) {
+    for (i in unlist(pkgs)) {
+        timestamp()
+        message('installing: ', i)
+        system(sprintf('R CMD INSTALL %s', i))
+    }
 }
