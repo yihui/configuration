8b6a955e8b2b711a735ab697b286f9f03c4ae9e3
Yihui Xie <xie@yihui.name>
2010-12-06 09:46:44 -0600

    LyX and Sweave configuration

---
+++ lyx-sweave-config.R
@@ -0,0 +1,3 @@
+## the configuration script is not here because GitHub uses https
+## but R can not handle such connections by default
+source('http://gitorious.org/yihui/lyx-sweave/blobs/raw/master/lyx-sweave-config.R')
