15120323f18a7843e76e7bb09e0844dbaa876966
Yihui Xie <xie@yihui.name>
2012-07-15 14:12:02 -0400

    rm unused scripts

--- install-R-pkgs.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-update.packages(.libPaths()[1], ask = FALSE)
-install.packages(setdiff(c('ggplot2', 'animation', 'rgl', 'gWidgetsRGtk2', 'Rd2roxygen',
-                           'mapdata', 'aplpack', 'TeachingDemos', 'plotrix', 'vioplot',
-                           'cairoDevice', 'xtable'), .packages(TRUE)))
-
--- lyx-sweave-config.R
+++
@@ -1,3 +0,0 @@
-## the configuration script is not here because GitHub uses https
-## but R can not handle such connections by default
-source('http://gitorious.org/yihui/lyx-sweave/blobs/raw/master/lyx-sweave-config.R')
